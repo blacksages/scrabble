090bad535917794bf031f758bdd1b24023f72242
blacksages <blacksages@live.fr>
2022-04-26 21:27:19 +0200

    tries et triescrabble finis

--- Trie.c
+++ Trie.c
@@ -3,36 +3,168 @@
 
 #include "Dict.h"
 
+typedef struct Node_t
+{
+    struct Node_t *children;
+    struct Node_t *parent;
+    struct Node_t *nextchild;
+    char label;
+    char *data;
+} Node;
 
-struct Dict_t {
-    // à compléter
+struct Dict_t
+{
+    Node *root;
+    Node **array;
+    size_t size;
+    size_t nbkeys;
 };
 
+static Node *dictGet(Dict *d, const char *key);
+static void init_node(Node *node, Node *children, Node *parent, Node *nextchild, char key, char *data);
+static void append_node(Dict *d, Node *node);
+static const char *dictSearchLongest_rec(Node *children, const char *letters, const char *longest);
 
-Dict* dictCreateEmpty() {
-    // à compléter
+static Node *dictGet(Dict *d, const char *key)
+{
+    size_t index = 0;
+    Node *next_node = d->root;
+    while (next_node && key[index] != '\0') // Max strlen(key)
+    {
+        next_node = next_node->children; // On regarde les prochaines lettres stockées
+        if (!next_node)
+            break;
+        while (next_node) // Max 26 tours (nbre de char possibles)
+        {
+            if (next_node->label == key[index])
+                break;
+            else
+                next_node = next_node->nextchild;
+        }
+        index++; // On regarde la prochaine lettre de "key"
+    }
+    return next_node;
 }
 
-size_t dictNbKeys(Dict* d) {
-    // à compléter
+static void init_node(Node *node, Node *children, Node *parent, Node *nextchild, char label, char *data)
+{
+    node->children = children;
+    node->parent = parent;
+    node->nextchild = nextchild;
+    node->label = label;
+    node->data = data;
 }
 
-void dictFree(Dict* d) {
-    // à compléter
+static void append_node(Dict *d, Node *node)
+{
+    if (d->nbkeys + 1 == d->size)
+    {
+        d->size = 2 * d->size; // On multiplie pour avoir une complexité linéaire
+        d->array = realloc(d->array, d->size * sizeof(Node *));
+    }
+    d->array[d->nbkeys] = node;
+    d->nbkeys++;
 }
 
-int dictContains(Dict* d, const char* key) {
-    // à compléter
+Dict *dictCreateEmpty()
+{
+    Dict *dict = malloc(sizeof(Dict));
+    Node *root = malloc(sizeof(Node));
+    init_node(root, NULL, NULL, NULL, '\0', NULL);
+
+    dict->root = root;
+    dict->size = 10;
+    dict->array = calloc(dict->size, sizeof(Node *));
+    dict->nbkeys = 1;
+    dict->array[0] = root;
+    return dict;
 }
 
-void* dictSearch(Dict* d, const char* key) {
-    // à compléter
+size_t dictNbKeys(Dict *d)
+{
+    return d->nbkeys;
 }
 
-void* dictSearchLongest(Dict* d, const char* letters) {
-    // à compléter
+void dictFree(Dict *d)
+{
+    for (size_t i = d->nbkeys; i != 0; i--)
+        free(d->array[i - 1]);
+    free(d->array);
+    free(d);
 }
 
-void dictInsert(Dict* d, const char* key, void* data) {
-    // à compléter
+int dictContains(Dict *d, const char *key)
+{
+    return dictSearch(d, key) != NULL;
+}
+
+void *dictSearch(Dict *d, const char *key)
+{
+    Node *node = dictGet(d, key);
+    return node ? node->data : NULL; // NULL si aucun mot n'a été trouvé
+}
+
+static const char *dictSearchLongest_rec(Node *children, const char *letters, const char *longest)
+{
+    if (strcmp(letters, "") == 0 || children == NULL)
+        return longest;
+    Node *child = children;
+    char *subletters = NULL;
+    const char *candidate = NULL;
+    while (child)
+    {
+        subletters = strchr(letters, child->label);
+        if (subletters)
+        {
+            if (child->data && strlen(child->data) > strlen(longest))
+                longest = child->data;
+            candidate = dictSearchLongest_rec(child->children, subletters + 1, longest);
+            if (strlen(candidate) > strlen(longest))
+                longest = candidate;
+        }
+        child = child->nextchild;
+    }
+    return longest;
+}
+
+void *dictSearchLongest(Dict *d, const char *letters)
+{
+    const char *longest = dictSearchLongest_rec(d->root->children, letters, "");
+    return strcmp(longest, "") == 0 ? NULL : longest;
+}
+
+void dictInsert(Dict *d, const char *key, void *data)
+{
+    Node *node = dictGet(d, key);
+    if (!node)
+    {
+        size_t index = 0;
+        Node *next_node = d->root, *prev_child = NULL, *parent_node = NULL, *new_node = NULL;
+        while (key[index] != '\0') // Max strlen(key)
+        {
+            parent_node = next_node;
+            next_node = parent_node->children; // On regarde les prochaines lettres stockées
+            while (next_node)                  // Max 26 tours (nbre de char possibles)
+            {
+                if (next_node->label == key[index])
+                    break;
+                prev_child = next_node;
+                next_node = next_node->nextchild;
+            }
+            if (!next_node)
+            {
+                new_node = malloc(sizeof(Node));
+                init_node(new_node, NULL, parent_node, NULL, key[index], NULL);
+                if (key[index + 1] == '\0') // Est-on à la dernière lettre de "key"?
+                    new_node->data = data;
+                if (parent_node->children) // Pas le premier fils
+                    prev_child->nextchild = new_node;
+                else // Premier fils
+                    parent_node->children = new_node;
+                append_node(d, new_node);
+                next_node = new_node; // On prépare la prochaine itération
+            }
+            index++; // Prochaine lettre de "key"
+        }
+    }
 }
--- TrieScrabble.c
+++ TrieScrabble.c
@@ -6,20 +6,109 @@
 #include "LinkedList.h"
 #include "Scrabble.h"
 
-
-struct ScrabbleDict_t {
-    // à compléter
+struct ScrabbleDict_t
+{
+    Dict *dict;
 };
 
-
-ScrabbleDict* scrabbleCreateDict(List* words) {
-    // à compléter
+////////////////////////////////////////////
+// heapsort for strings
+static void swap_char(char *tab, int index_1, int index_2)
+{
+    char tmp = tab[index_1];
+    tab[index_1] = tab[index_2];
+    tab[index_2] = tmp;
 }
 
-void scrabbleFreeDict(ScrabbleDict* sd) {
-    // à compléter
+static void max_heapify_char(char *array, int i, int heap_size)
+{
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    int largest = i;
+    if (l < heap_size && array[l] > array[i])
+        largest = l;
+    /*
+    else
+      largest = i;
+    */
+    if (r < heap_size && array[r] > array[largest])
+        largest = r;
+
+    if (largest != i)
+    {
+        swap_char(array, i, largest);
+        max_heapify_char(array, largest, heap_size);
+    }
 }
 
-char* scrabbleFindLongestWord(ScrabbleDict* sd, const char* letters) {
-    // à compléter
+static void build_max_heap_char(char *array, int array_size)
+{
+    int heap_size = array_size;
+    int i;
+    for (i = array_size / 2 - 1; i >= 0; i--)
+        max_heapify_char(array, i, heap_size);
+}
+
+static void heapsort_char(char *array, int array_size)
+{
+    build_max_heap_char(array, array_size);
+    int i;
+    for (i = array_size - 1; i >= 0; i--)
+    {
+        swap_char(array, i, 0);
+        max_heapify_char(array, 0, i);
+    }
+}
+////////////////////////////////////////////
+
+ScrabbleDict *scrabbleCreateDict(List *words)
+{
+    Dict *dict = dictCreateEmpty();
+    Node *node = llHead(words);
+    char *data = NULL;
+    char *keys;
+    size_t *lens_including_null = calloc(llLength(words), sizeof(size_t));
+    size_t total_len = 0, offset = 0;
+    size_t i = 0;
+    while (node) // On cherche à avoir toutes les clés en une fois pour éviter des malloc/free en boucle
+    {
+        lens_including_null[i] = strlen((char *)llData(node)) + 1;
+        total_len += lens_including_null[i];
+        node = llNext(node);
+        i++;
+    }
+    keys = calloc(total_len, sizeof(char));
+    node = llHead(words);
+    i = 0;
+    while (node) // Insertion des couples clés/données
+    {
+        data = (char *)llData(node);
+        strcpy(keys + offset, data);
+        heapsort_char(keys + offset, lens_including_null[i] - 1);
+        dictInsert(dict, keys + offset, data);
+        node = llNext(node);
+        offset += lens_including_null[i];
+        i++;
+    }
+    free(lens_including_null); // Au final 2 free et 2 calloc plutôt de que n free et n malloc
+    free(keys);
+    ScrabbleDict *sd = malloc(sizeof(ScrabbleDict));
+    sd->dict = dict;
+    return sd;
+}
+
+void scrabbleFreeDict(ScrabbleDict *sd)
+{
+    dictFree(sd->dict);
+}
+
+char *scrabbleFindLongestWord(ScrabbleDict *sd, const char *letters)
+{
+    size_t len = strlen(letters);
+    char *sorted_l = malloc((len + 1) * sizeof(char));
+    strcpy(sorted_l, letters);
+    heapsort_char(sorted_l, len);
+    char *longest = dictSearchLongest(sd->dict, sorted_l);
+    free(sorted_l);
+    return longest;
 }
